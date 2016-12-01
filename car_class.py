b09b7f092664cee0b73582bd55a893a283162538
Kajabukama <kajabukama@gmail.com>
2016-12-01 23:21:24 +0300

    Added a Car class and a test folder

---
+++ car_class.py
@@ -0,0 +1,24 @@
+''' 
+	Car class
+	with un implemented methods
+'''
+
+class Car(object):
+
+	def __init__(self):
+		self.color = color
+		self.engine_type = engine_type
+		self.number_doors = number_doors;
+		self.transimission = transimission
+
+	def set_color(self, color):
+		self.color = color
+
+	def get_color(self):
+		return self.color
+
+	def set_engine(self, engine_type):
+		self.engine_type = engine_type
+
+	def get_engine_type(self):
+		return self.engine_type
